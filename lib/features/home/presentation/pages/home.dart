75c67f23672cd971b5ba1eb5d12a42de2aac1bef
Pedro Remedios <pedro.martins.remedios@fidelidade.pt>
2025-05-25 21:06:17 +0100

    Main UI changes

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -11,17 +11,6 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Dashboard')),
-      body: const Center(child: Text('Welcome to Homepage!')),
-      floatingActionButton: FloatingActionButton(
-        shape: CircleBorder(),
-        backgroundColor: Colors.blue,
-        onPressed: () {
-          // Add your onPressed code here!
-        },
-        child: Padding(padding: const EdgeInsets.all(12.0), child: SvgPicture.asset('assets/SVG/blind.svg', semanticsLabel: 'Start Walk Icon', color: Colors.white)),
-      ),
-    );
+    return Container();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
 
-  static const List<Widget> _pages = <Widget>[Homepage(), Center(child: Text('Walks')), Center(child: Text('Bags')), Center(child: Text('Profile'))];
+  List pageTitles = ['Dashboard', 'Bags', '', 'Events', 'Profile'];
+
+  ColorFilter iconColorFilter = ColorFilter.mode(Colors.black, BlendMode.srcIn);
+
+  var startWalkIconAsset = SvgPicture.asset('assets/SVG/blind.svg', semanticsLabel: 'Start Walk Icon', colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn));
+
+  static final List<Widget> _pages = <Widget>[Homepage(), Center(child: Text('Walks')), Center(child: Text('Bags')), Container(), Center(child: Text('Profile')), Center(child: Text('Profile'))];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -60,19 +66,43 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
+      appBar: AppBar(title: Text(pageTitles[_selectedIndex])),
       body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
         items: [
-          BottomNavigationBarItem(icon: SizedBox(height: 24, width: 24, child: SvgPicture.asset('assets/SVG/paw.svg', semanticsLabel: 'Home', color: Colors.black)), label: 'Home'),
-          BottomNavigationBarItem(icon: SizedBox(height: 24, width: 24, child: SvgPicture.asset('assets/SVG/blind.svg', semanticsLabel: 'Bags', color: Colors.black)), label: 'Walks'),
-          BottomNavigationBarItem(icon: SizedBox(height: 24, width: 24, child: SvgPicture.asset('assets/SVG/walking-the-dog.svg', semanticsLabel: 'Walks', color: Colors.black)), label: 'Bags'),
-          BottomNavigationBarItem(icon: SizedBox(height: 24, width: 24, child: SvgPicture.asset('assets/SVG/user.svg', semanticsLabel: 'Profile', color: Colors.black)), label: 'Profile'),
+          BottomNavigationBarItem(icon: SizedBox(height: 24, width: 24, child: SvgPicture.asset('assets/SVG/paw.svg', semanticsLabel: 'Home', colorFilter: iconColorFilter)), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: SizedBox(height: 24, width: 24, child: SvgPicture.asset('assets/SVG/walking-the-dog.svg', semanticsLabel: 'Bags', colorFilter: iconColorFilter)),
+            label: 'Bags',
+          ),
+          BottomNavigationBarItem(icon: SizedBox(height: 24, width: 24, child: Container()), label: ''),
+          BottomNavigationBarItem(
+            icon: SizedBox(height: 24, width: 24, child: SvgPicture.asset('assets/SVG/walking-the-dog.svg', semanticsLabel: 'Events', colorFilter: iconColorFilter)),
+            label: 'Bags',
+          ),
+          BottomNavigationBarItem(icon: SizedBox(height: 24, width: 24, child: SvgPicture.asset('assets/SVG/user.svg', semanticsLabel: 'Profile', colorFilter: iconColorFilter)), label: 'Profile'),
         ],
         type: BottomNavigationBarType.fixed,
       ),
+      floatingActionButton: Container(
+        margin: EdgeInsets.only(left: 0.0, top: 16.0, right: 0.0, bottom: 0.0),
+        child: SizedBox(
+          height: 70,
+          width: 70,
+          child: FloatingActionButton(
+            shape: CircleBorder(side: BorderSide(color: Colors.white, width: 2)),
+            backgroundColor: Colors.blue,
+            onPressed: () {
+              // Add your onPressed code here!
+            },
+            elevation: 0,
+            child: Padding(padding: const EdgeInsets.all(18.0), child: startWalkIconAsset),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
